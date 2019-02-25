edb8f812164ed7d2ab50ae4f77e952958c99c0f2
xiongguojiang <xiongguojiang@didiglobal.com>
2019-02-25 15:59:36 +0800

    基本文件

--- src/test/java/com/neu/x/leetcode/LeetcodeApplicationTests.java
+++ src/test/java/com/neu/x/leetcode/LeetcodeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.neu.x.leetcodejava;
+package com.neu.x.leetcode;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LeetcodeJavaApplicationTests {
+public class LeetcodeApplicationTests {
 
 	@Test
 	public void contextLoads() {
