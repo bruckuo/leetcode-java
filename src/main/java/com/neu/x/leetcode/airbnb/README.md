226a796b0b5d07ee492ace0c411b5dd0524758c3
xiongguojiang <xiongguojiang@didiglobal.com>
2019-02-25 17:38:00 +0800

    solution

---
+++ src/main/java/com/neu/x/leetcode/airbnb/README.md
@@ -0,0 +1,2 @@
+### Airbnb算法题
+[github java实现](https://github.com/cicean/leetcode-java/tree/98530981aecbd75bf482fd22df2cfc553690c104/src/Airbnb)
---
+++ src/main/java/com/neu/x/leetcode/findMedianSortedArrays/Solution.java
@@ -0,0 +1,56 @@
+package com.neu.x.leetcode.findMedianSortedArrays;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @ description:滑动窗口
+ *
+ * 复杂度分析
+
+ * 时间复杂度：O(2n) = O(n)O(2n)=O(n)，在最糟糕的情况下，每个字符将被 ii 和 jj 访问两次。
+
+ * 空间复杂度：O(min(m, n))O(min(m,n))，与之前的方法相同。滑动窗口法需要 O(k)O(k) 的空间，其中 kk 表示 Set 的大小。而Set的大小取决于字符串 nn 的大小以及字符集/字母 mm 的大小。
+ *
+ * @ author: guojiang.x
+ * @ created: 2019-02-25 16:21
+ */
+public class Solution {
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j));
+                j++;
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i));
+                i++;
+            }
+        }
+        return ans;
+    }
+
+    public int lengthOfLongestSubstring1(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+
+    public static void main (String[] args) {
+        System.out.println(lengthOfLongestSubstring("ssytecsgs"));
+    }
+}
