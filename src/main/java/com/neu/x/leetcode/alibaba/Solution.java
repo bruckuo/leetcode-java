4980a9d05a3b978771e0074a64e02780b6fe2256
xiongguojiang@163.com <seany102568>
2019-03-13 10:48:42 +0800

    链表

---
+++ src/main/java/com/neu/x/leetcode/alibaba/Solution.java
@@ -0,0 +1,9 @@
+package com.neu.x.leetcode.alibaba;
+
+/**
+ * @ description: 题
+ * @ author: xiongguojiang
+ * @ created: 2019-03-13 上午9:10
+ */
+public class Solution {
+}
---
+++ src/main/java/com/neu/x/leetcode/listNode/SwapPairsSolution.java
@@ -0,0 +1,138 @@
+package com.neu.x.leetcode.listNode;
+
+/**
+ * @ description: ListNode
+ * @ author: jiangdongxue
+ * @ created: 2019-03-12 下午10:18
+ */
+public class SwapPairsSolution {
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    /**
+     * 两两交换链表中的节点
+     * @param head node
+     * @return ListNode
+     */
+    public static ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+
+    /**
+     * 删除链表的倒数第N个节点
+     * @param head node
+     * @param n 删除节点
+     * @return ListNode
+     */
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        // Advances first pointer so that the gap between first and second is n nodes apart
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+        // Move first to the end, maintaining the gap
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+
+    /**
+     * 合并两个有序链表
+     * @param l1 node1
+     * @param l2 node2
+     * @return ListNode
+     */
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // 类似归并排序中的合并过程
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            }
+        }
+        // 任一为空，直接连接另一条链表
+        if (l1 == null) {
+            cur.next = l2;
+        } else {
+            cur.next = l1;
+        }
+        return dummyHead.next;
+    }
+
+    /**
+     * 单链表反转
+     * 解答 递归，在反转当前节点之前先反转后续节点
+     * @param head node
+     * @return ListNode
+     */
+    public static ListNode reverse1(ListNode head) {
+        // head看作是前一结点，head.getNext()是当前结点，reHead是反转后新链表的头结点
+        if (head == null || head.next == null) {
+            return head;// 若为空链或者当前结点在尾结点，则直接还回
+        }
+        ListNode reHead = reverse1(head.next);// 先反转后续节点head.getNext()
+        head.next = head;// 将当前结点的指针域指向前一结点
+        head.next = null;// 前一结点的指针域令为null;
+        return reHead;// 反转后新链表的头结点
+    }
+
+    /**
+     * 单链表反转
+     * 解答 递归，在反转当前节点之前先反转后续节点
+     * @param head node
+     * @return ListNode
+     */
+    public static ListNode reverse2(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode pre = head;// 上一结点
+        ListNode cur = head.next;// 当前结点
+        ListNode tmp;// 临时结点，用于保存当前结点的指针域（即下一结点）
+        while (cur != null) {// 当前结点为null，说明位于尾结点
+            tmp = cur.next;
+            cur.next = pre;// 反转指针域的指向
+            // 指针往下移动
+            pre = cur;
+            cur = tmp;
+        }
+        // 最后将原链表的头节点的指针域置为null，还回新链表的头结点，即原链表的尾结点
+        head.next = null;
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(0);
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        node.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        ListNode result = reverse1(node);
+        System.out.print(result.val);
+    }
+}
